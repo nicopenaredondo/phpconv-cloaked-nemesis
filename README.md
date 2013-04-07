f0ff24da6ac77932d3d5a460c655296d6c18c62e
Nico R. Penaredondo <nico.penaredondo@live.com>
2013-04-07 13:37:36 +0800

    'yolo'

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 WALA LANG GUSTO KO LANG I PUSH DITO SA GITHUB..SA OFFICE AKO MAGCCODE MEHEHEHEHE
 =======================
 
-YOLO
-=======================
-
-SWAG
+STRING MANIPULATION NALANG!!!
 =======================
--- phpconv/index.php
+++ phpconv/index.php
@@ -1,37 +1,19 @@
-<?php
-require_once 'Classes/PHPExcel.php';
-require_once 'Classes/PHPExcel/IOFactory.php';
-
-$objPHPExcel = new PHPExcel();
-
-//set properties
-$objPHPExcel->getProperties()
-			    ->setCreator("Ronny Boland")
-			    ->setLastModifiedBy("Nico R Penaredondo")
-			    ->setTitle("PHP Conversion")
-			    ->setSubject("This is a f*cking PHP Conversion")
-			    ->setDescription("Made from PHP Excel")
-			    ->setCategory("Test test conversion php")
-			    ->setKeywords("PHP SHORE24");
-
-$objPHPExcel->getActiveSheet()->setTitle("Test Demo");
-
-$objPHPExcel->setActiveSheetIndex()
-			    ->setCellValueByColumnAndRow('0','1','_type')
-			    ->setCellValueByColumnAndRow('1','1','_attribute')
-			    ->setCellValueByColumnAndRow('2','1','_category')
-			    ->setCellValueByColumnAndRow('3','1','tax_class_id')
-			    ->setCellValueByColumnAndRow('4','1','status')
-			    ->setCellValueByColumnAndRow('5','1','weight')
-			    ->setCellValueByColumnAndRow('6','1','sku')
-			    ->setCellValueByColumnAndRow('7','1','name')
-			    ->setCellValueByColumnAndRow('8','1','price')
-			    ->setCellValueByColumnAndRow('9','1','description')
-			    ->setCellValueByColumnAndRow('10','1','short_description')
-			    ->setCellValueByColumnAndRow('11','1','visibility')
-			    ->setCellValueByColumnAndRow('12','1','qty')
-			    ->setCellValueByColumnAndRow('13','1','is_in_stock')
-			    ->setCellValueByColumnAndRow('14','1','manage_stock');
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel,'Excel2007');
-$objWriter->save('MyExcel.csv');
-
+<?php require_once 'functions.php';?>
+<html> 
+<body>
+	<?php if(isset($_POST['submit'])):?>
+	<?php
+		read_file(upload_file());
+		
+		?>
+	<?php else:?>
+	
+  <form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+    <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
+	<input name="uploaded_file" type="file" />
+    <br>
+    <input type="submit" value="Upload"  name="submit" />
+  </form> 
+  <?php endif;?>
+</body> 
+</html>
